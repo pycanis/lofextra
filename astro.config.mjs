351e84f9e5f809630d6a94e9ee8ef71439869284
pycan-jouza <pycan@proton.me>
2024-07-25 23:18:40 +0200

    Try fix PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import react from "@astrojs/react";
 import AstroPWA from "@vite-pwa/astro";
 import { defineConfig } from "astro/config";
-import manifest from "./manifest.webmanifest";
+import manifest from "./manifest.json";
 
 // https://astro.build/config
 export default defineConfig({
